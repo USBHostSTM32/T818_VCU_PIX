2ef2013225141815d62555fbfb5c654508233ce8
Quantum095 <105911822+Quantum095@users.noreply.github.com>
2024-06-28 12:26:52 +0200

    check code and doxygen

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -59,7 +59,7 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 
 /* USER CODE BEGIN Private defines */
-
+void print(char *text);
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
