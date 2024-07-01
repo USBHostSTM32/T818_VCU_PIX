951516de6c0e15ca768c6cb2981cf8908802086c
Quantum095 <105911822+Quantum095@users.noreply.github.com>
2024-07-01 11:56:36 +0200

    Can receive fixed, doxygen added

--- Core/Src/can.c
+++ Core/Src/can.c
@@ -54,19 +54,22 @@ void MX_CAN1_Init(void) {
 	/* USER CODE BEGIN CAN1_Init 2 */
 	CAN_FilterTypeDef canfilterconfig;
 	canfilterconfig.FilterActivation = CAN_FILTER_ENABLE;
-	canfilterconfig.FilterBank = 10;
+	canfilterconfig.FilterBank = 0;
 	canfilterconfig.FilterFIFOAssignment = CAN_RX_FIFO0;
-	canfilterconfig.FilterIdHigh = 0;//here
+	canfilterconfig.FilterIdHigh = 0x0193<<5;
 	canfilterconfig.FilterIdLow = 0x0000;
-	canfilterconfig.FilterMaskIdHigh = 0;//here
+	canfilterconfig.FilterMaskIdHigh = 0xFFE0;
 	canfilterconfig.FilterMaskIdLow = 0x0000;
 	canfilterconfig.FilterMode = CAN_FILTERMODE_IDMASK;
 	canfilterconfig.FilterScale = CAN_FILTERSCALE_32BIT;
-	canfilterconfig.SlaveStartFilterBank = 14;
+	//canfilterconfig.SlaveStartFilterBank = 14;
 
-	if (HAL_CAN_ConfigFilter(&hcan1, &canfilterconfig) != HAL_OK) {
+	if (HAL_CAN_ConfigFilter(&hcan1, &canfilterconfig) != HAL_OK)
+	{
+		/* Filter configuration Error */
 		Error_Handler();
 	}
+	/* USER CODE END CAN1_Init 2 */
 
 }
 
@@ -92,6 +95,9 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef *canHandle) {
 		GPIO_InitStruct.Alternate = GPIO_AF9_CAN1;
 		HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
+		/* CAN1 interrupt Init */
+		HAL_NVIC_SetPriority(CAN1_RX0_IRQn, 5, 0);
+		HAL_NVIC_EnableIRQ(CAN1_RX0_IRQn);
 		/* USER CODE BEGIN CAN1_MspInit 1 */
 
 		/* USER CODE END CAN1_MspInit 1 */
@@ -113,6 +119,8 @@ void HAL_CAN_MspDeInit(CAN_HandleTypeDef *canHandle) {
 		 */
 		HAL_GPIO_DeInit(GPIOD, GPIO_PIN_0 | GPIO_PIN_1);
 
+		/* CAN1 interrupt Deinit */
+		HAL_NVIC_DisableIRQ(CAN1_RX0_IRQn);
 		/* USER CODE BEGIN CAN1_MspDeInit 1 */
 
 		/* USER CODE END CAN1_MspDeInit 1 */
